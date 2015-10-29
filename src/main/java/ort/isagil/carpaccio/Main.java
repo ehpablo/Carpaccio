8c4459fa2c5b0a44f4ca2a74461a4cc174fb3ad7
alumnoFI <alumnoFI@FIW7LC0515.facultades.ort.edu.uy>
2015-10-29 21:24:35 -0200

    comentario agregado

---
+++ src/main/java/ort/isagil/carpaccio/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ort.isagil.carpaccio;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author alumnoFI
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int primerEntrada;
+        while(true){
+            System.out.println("Ingrese valor:");
+            primerEntrada = scanner.nextInt();
+        }
+    }
+}
